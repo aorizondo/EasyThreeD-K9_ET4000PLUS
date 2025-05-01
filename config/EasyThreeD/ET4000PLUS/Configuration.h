e8d9d7dbb59fa93ab6e21653ef8201baf055e120
Antonio Orizondo Leyva <aorizondoleyva@gmail.com>
2025-05-01 08:41:04 -0400

    set dimentions to 90mm on all axis

--- config/EasyThreeD/ET4000PLUS/Configuration.h
+++ config/EasyThreeD/ET4000PLUS/Configuration.h
@@ -1726,8 +1726,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 100
-#define Y_BED_SIZE 100
+#define X_BED_SIZE 90
+#define Y_BED_SIZE 90
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1735,7 +1735,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 100
+#define Z_MAX_POS 900
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
