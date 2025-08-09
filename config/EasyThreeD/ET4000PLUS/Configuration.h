83eb675d74f35ca724e0adde9843768296f05ec7
Antonio Orizondo Leyva <aorizondoleyva@gmail.com>
2025-08-09 16:23:47 -0400

    real bed adjustment to 120x120 mm, printing area 100x100

--- config/EasyThreeD/ET4000PLUS/Configuration.h
+++ config/EasyThreeD/ET4000PLUS/Configuration.h
@@ -1726,15 +1726,15 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 100
-#define Y_BED_SIZE 100
+#define X_BED_SIZE 120
+#define Y_BED_SIZE 120
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS 10
+#define Y_MIN_POS 10
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 110
+#define Y_MAX_POS 110
 #define Z_MAX_POS 100
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
